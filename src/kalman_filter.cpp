11cd8c463113f85bfda83d8484be9d326822daae
DANIEL <danielocampovalencia@gmail.com>
2020-07-30 22:41:23 -0400

    Update kalman_filter.cpp

--- src/kalman_filter.cpp
+++ src/kalman_filter.cpp
@@ -3,98 +3,93 @@
 using Eigen::MatrixXd;
 using Eigen::VectorXd;
 
-/*
-* Please note that the Eigen library does not initialize
-*   VectorXd or MatrixXd objects with zeros upon creation.
-*/
+/* 
+ * Please note that the Eigen library does not initialize 
+ *   VectorXd or MatrixXd objects with zeros upon creation.
+ */
 
 KalmanFilter::KalmanFilter() {}
 
 KalmanFilter::~KalmanFilter() {}
 
 void KalmanFilter::Init(VectorXd &x_in, MatrixXd &P_in, MatrixXd &F_in,
-	MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
-	x_ = x_in;
-	P_ = P_in;
-	F_ = F_in;
-	H_ = H_in;
-	R_ = R_in;
-	Q_ = Q_in;
+                        MatrixXd &H_in, MatrixXd &R_in, MatrixXd &Q_in) {
+  x_ = x_in;
+  P_ = P_in;
+  F_ = F_in;
+  H_ = H_in;
+  R_ = R_in;
+  Q_ = Q_in;
 }
 
 void KalmanFilter::Predict() {
-	/**
-	* TODO: predict the state
-	*/
-	x_ = F_ * x_;
-	MatrixXd Ft = F_.transpose();
-	P_ = F_ * P_ * Ft + Q_;
+  /**
+   * TODO: predict the state
+   */
+  x_ = F_ * x_;
+  MatrixXd Ft = F_.transpose();
+  P_ = F_ * P_ * Ft + Q_;
 }
 
 void KalmanFilter::Update(const VectorXd &z) {
-	/**
-	* TODO: update the state by using Kalman Filter equations
-	*/
-	VectorXd z_pred = H_ * x_;
-	VectorXd y = z - z_pred;
-	//Mesaurement updates
-	MatrixXd Ht = H_.transpose();
-	MatrixXd S = H_ * P_ * Ht + R_;
-	MatrixXd Si = S.inverse();
-	MatrixXd PHt = P_ * Ht;
-	MatrixXd K = PHt * Si;
-
-	//new estimate
-	x_ = x_ + (K * y);
-	long x_size = x_.size();
-	MatrixXd I = MatrixXd::Identity(x_size, x_size);
-	P_ = (I - K * H_) * P_;
+  /**
+   * TODO: update the state by using Kalman Filter equations
+   */
+  VectorXd z_pred = H_ * x_;
+  VectorXd y = z - z_pred;
+  
+  //Mesaurement updates
+  MesaurementUpdates(y);
 }
 
 void KalmanFilter::UpdateEKF(const VectorXd &z) {
-	/**
-	* TODO: update the state by using Extended Kalman Filter equations
-	*/
-	float px = x_[0];
-	float py = x_[1];
-	float vx = x_[2];
-	float vy = x_[3];
+  /**
+   * TODO: update the state by using Extended Kalman Filter equations
+   */
+  float px = x_[0];
+  float py = x_[1];
+  float vx = x_[2];
+  float vy = x_[3];
 
-	//Checking the value is not zero
-	if (px == 0. && py == 0.)
-		return;
+  //Checking the value is not zero
+  if(px == 0. && py == 0.)
+    return;
+  
+  float rho = sqrt(px*px + py*py);
+  float theta = atan2(py, px);
+  
+  //Checking the value is not zero
+  if (rho < 0.0001) {
+    rho = 0.0001;
+  } 
+  float rho_dot = (px*vx + py*vy) / rho;
+  
 
-	float rho = sqrt(px*px + py*py);
-	float theta = atan2(py, px);
+  //Finding h(x)
+  VectorXd h = VectorXd(3); // h(x_)
+  h << rho, theta, rho_dot;
+  VectorXd y = z-h;
 
-	//Checking the value is not zero
-	if (rho < 0.0001) {
-		rho = 0.0001;
-	}
-	float rho_dot = (px*vx + py*vy) / rho;
+  //Normalize the angle between -pi to pi
+  while (y[1] < -M_PI)
+    y[1] += 2 * M_PI;
+  while (y[1] > M_PI)
+    y[1] -= 2 * M_PI;
+  
+  //Mesaurement updates
+  MesaurementUpdates(y);
+}
+void KalmanFilter::MesaurementUpdates(const VectorXd &y) {
+  //Mesaurement updates
+  MatrixXd Ht = H_.transpose();
+  MatrixXd S = H_ * P_ * Ht + R_;
+  MatrixXd Si = S.inverse();
+  MatrixXd PHt = P_ * Ht;
+  MatrixXd K = PHt * Si;
 
-
-	//Finding h(x)
-	VectorXd h = VectorXd(3); // h(x_)
-	h << rho, theta, rho_dot;
-	VectorXd y = z - h;
-
-	//Normalize the angle between -pi to pi
-	while (y[1] < -M_PI)
-		y[1] += 2 * M_PI;
-	while (y[1] > M_PI)
-		y[1] -= 2 * M_PI;
-
-	//Mesaurement updates
-	MatrixXd Ht = H_.transpose();
-	MatrixXd S = H_ * P_ * Ht + R_;
-	MatrixXd Si = S.inverse();
-	MatrixXd PHt = P_ * Ht;
-	MatrixXd K = PHt * Si;
-
-	//new estimate
-	x_ = x_ + (K * y);
-	long x_size = x_.size();
-	MatrixXd I = MatrixXd::Identity(x_size, x_size);
-	P_ = (I - K * H_) * P_;
+  //new estimate
+  x_ = x_ + (K * y);
+  long x_size = x_.size();
+  MatrixXd I = MatrixXd::Identity(x_size, x_size);
+  P_ = (I - K * H_) * P_;
 }
